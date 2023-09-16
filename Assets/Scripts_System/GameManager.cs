7c9e8552001d215eebf58bbf3d37ed7f49f94b1c
RenzoSuganuma <discoverygworks2023@gmail.com>
2023-09-16 16:33:24 +0900

    経過時間（ステージ）の表示実装

--- Assets/Scripts_System/GameManager.cs
+++ Assets/Scripts_System/GameManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 /// <summary>
 /// プレイヤーのステータスとゲームのステータスを管理するクラス
 /// </summary>
@@ -9,13 +10,19 @@ public class GameManager : MonoBehaviour
     [SerializeField] float _playerHealth;
     [SerializeField] Material _pDefaultMaterial;
     [SerializeField] Material _pSpawnMaterial;
+    [SerializeField] Text _elapsedTimeText;
+    [SerializeField] Text _playerNameText;
+    float _elapsedTime = 0;
     private void Start()
     {
         GameObject.FindGameObjectWithTag("Player").GetComponent<Renderer>().material = _pSpawnMaterial;
+        _elapsedTime = 0;
+        _playerNameText.text = "P.Name : " + "Ethan.W";
     }
     private void Update()
     {
-        
+        _elapsedTime += Time.deltaTime;
+        _elapsedTimeText.text = _elapsedTime.ToString("F3");
     }
     /// <summary>体力を更新するメソッド。引数に加算する値を代入</summary>
     /// <param name="health"></param>
