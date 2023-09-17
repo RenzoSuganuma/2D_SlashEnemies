9945935b61cb250bd9fee2e314dc956b57e78a12
RenzoSuganuma <discoverygworks2023@gmail.com>
2023-09-17 14:40:48 +0900

    リスポーン処理実装

--- Assets/Scripts_System/GameManager.cs
+++ Assets/Scripts_System/GameManager.cs
@@ -10,6 +10,7 @@ public class GameManager : MonoBehaviour
     [SerializeField] float _playerHealth;
     [SerializeField] Material _pDefaultMaterial;
     [SerializeField] Material _pSpawnMaterial;
+    [SerializeField] Transform _pSpawnTransform;
     [SerializeField] Text _elapsedTimeText;
     [SerializeField] Slider _hpSlider;
     float _elapsedTime = 0;
@@ -21,8 +22,20 @@ public class GameManager : MonoBehaviour
     private void Update()
     {
         _elapsedTime += Time.deltaTime;
-        _elapsedTimeText.text = _elapsedTime.ToString("F3");
+        _elapsedTimeText.text = _elapsedTime.ToString("F2");
         _hpSlider.value = _playerHealth / 100f;
+        if (_playerHealth <= 0)
+            Respawn();
+    }
+    /// <summary>プレイヤーのリスポーン処理</summary>
+    private void Respawn()
+    {
+        var p = GameObject.FindGameObjectWithTag("Player");
+        p.SetActive(false);
+        p.transform.position = _pSpawnTransform.position;
+        _playerHealth = 100;
+        _elapsedTime += 2.0f;
+        p.SetActive(true);
     }
     /// <summary>体力を更新するメソッド。引数に加算する値を代入</summary>
     /// <param name="health"></param>
@@ -30,6 +43,9 @@ public class GameManager : MonoBehaviour
     {
         _playerHealth += health;
     }
+    /// <summary>スポーンしたばかりの状態のシェーダーに切り替え</summary>
+    /// <param name="sec"></param>
+    /// <returns></returns>
     IEnumerator GodMode(float sec)
     {
         GameObject.FindGameObjectWithTag("Player").GetComponent<Renderer>().material = _pSpawnMaterial;
